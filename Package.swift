69b4725043d66b001b04143577699a8461a75bd8
Mahamadou DOUMBIA <mahamadou.doumbia@orangemali.com>
2025-11-20 10:01:14 +0000

    Release 0.1.3 with unified mode support

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OUIRuntime",
-            url: "https://github.com/molobala/ouid-ios/releases/download/0.1.2/OUIRuntime.xcframework.zip",
-            checksum: "46def6e4ee5da956e4b6d3539fc930b1de7efdce2f5ff2827bae9bb30ee1906f"
+            url: "https://github.com/molobala/ouid-ios/releases/download/0.1.3/OUIRuntime.xcframework.zip",
+            checksum: "e0e9cfc671d503fb76083cdae12cb98eaa1b8f76db8411c0a5398a3f012df6d7"
         )
     ]
 )
