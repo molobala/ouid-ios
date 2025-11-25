c654d2deb58463f616b481d8812ca4cc8ca82370
Mahamadou DOUMBIA <mahamadou.doumbia@orangemali.com>
2025-11-25 14:36:40 +0000

    fix 0.1.4 release

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OUIRuntime",
-            url: "https://github.com/molobala/ouid-ios/releases/download/0.1.3/OUIRuntime.xcframework.zip",
+            url: "https://github.com/molobala/ouid-ios/releases/download/0.1.4/OUIRuntime.xcframework.zip",
             checksum: "b9c297e2ba294f52058a2d8cbf8dc8b14019dba005b0e895f3fc050422a1a9d9"
         )
     ]
